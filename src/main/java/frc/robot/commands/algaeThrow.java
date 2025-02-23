3d3953107b6463135e6132bf65158c948e25542c
Samyoppp <sammycolewilson@gmail.com>
2025-02-23 01:29:35 -0500

    Fixed algae throw command

--- src/main/java/frc/robot/commands/algaeThrow.java
+++ src/main/java/frc/robot/commands/algaeThrow.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.RobotContainer;
 import frc.robot.Constants.PositionClass.Positions;
 import frc.robot.subsystems.IntakeSubsystem;
-public class algaeThrow extends ParallelCommandGroup{
+public class algaeThrow extends SequentialCommandGroup{
 
     double delay = 1; //amount of time to wait before throwing algae
 
@@ -21,14 +21,11 @@ public class algaeThrow extends ParallelCommandGroup{
 
     addCommands(
 
-        new moveToPosition(Positions.L4), //while moving to l4...
-
-        new SequentialCommandGroup(
-
-            new WaitCommand(delay), // wait a small amount of time until we're close to the top
-            new InstantCommand(RobotContainer.intake::ejectAlgae) //throw the algae with upwards momentum
+        new moveToPosition(Positions.L4), //while moving to l4 (top)...
+        new WaitCommand(delay), // wait a small amount of time until we're close to the top
+        new InstantCommand(RobotContainer.intake::ejectAlgae) //throw the algae with upwards momentum
             
-        )
+        //note: maybe also starting with arm down all the way -> flinging it up would make it have more momentum?
 
          );
     }
